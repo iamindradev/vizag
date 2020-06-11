a2bf29e54dc00c3d2561f4aa8b84812915968255
indradev <yadavdevendra99876@gmail.com>
2020-06-11 15:08:47 +0530

    added

--- frontend/form.js
+++ frontend/form.js
@@ -1,19 +1,18 @@
+
+
 function takeinput(){
     var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    
+var password = document.getElementById("password").value;
+sessionStorage.setItem('id', JSON.stringify(username));
     myobj={username: username, password: password} ;
     data= JSON.stringify(myobj);
     console.log(data)
-
     var xhttp = new XMLHttpRequest();
     xhttp.open("POST","http://127.0.0.1:8000/login/",true);
     xhttp.onreadystatechange = function()
     {
 
         console.log(xhttp.readyState)
-        
-        
         xhttp.onload = function(){
             if(this.readyState==4 && this.status === 200){
             console.log(this.responseText)
@@ -39,18 +38,7 @@ function takeinput(){
     
     xhttp.send(data)
     console.log(myobj)
-    var store=sessionStorage.setItem("lib",data);
-    console.log(store)
-
-}
-function givedata()
-    {
-        
-        student_data=data['b']
-        this.addEventListener("load",givedata);
-        alert('welcome'+student_data+'hey');
-        
-    }  
+} 
 function openhome(){
     window.open("homepage.html");
 }
